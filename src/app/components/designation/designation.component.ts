210bff45b429e086cf61dbf3d6d4f8913803ba7a
gurparshadsharma <gurparshadsharma97@gmail.com>
2025-03-13 15:42:24 +0200

    components added

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/role';
 
 @Component({
   selector: 'app-designation',
   imports: [],
   templateUrl: './designation.component.html',
-  styleUrl: './designation.component.css'
+  styleUrl: './designation.component.css',
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+  designationList: IDesignation[] = [];
+  isLoading: boolean = true;
+  masterService = inject(MasterService);
 
+  // TODO: add error handling here, use pipe, also add the loading state.
+  ngOnInit(): void {
+    this.masterService
+      .getDesignations()
+      .subscribe((result: APIResponseModel) => {
+        this.designationList = result.data;
+        this.isLoading = false;
+      });
+  }
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
 
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
+
+  getAllRoles() {
+    this.http
+      .get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles')
+      .subscribe((res: any) => {
+        this.roleList = res.data;
+      });
+  }
+
+  // inputType = 'checkbox';
+  // firstName = 'simon';
+  // selectedState: string = 'Please select state';
+  // isChecked: boolean = false;
+  // showWelcomeAlert() {
+  //   alert('welcome to the angular 18 tutorial');
+  // }
+  // showWelcomeAlert2(message: string) {
+  //   alert(message);
+  // }
+  // onSelectedStateChange() {
+  //   alert('state change');
+  // }
 }
