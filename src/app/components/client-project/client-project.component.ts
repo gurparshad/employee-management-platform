0a127f71cc3a3a34807acf4e42e72242d8473e84
gurparshadsharma <gurparshadsharma97@gmail.com>
2025-03-17 14:36:23 +0200

    feat: add validation to client-project form

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ClientService } from '../../services/client.service';
 import { APIResponseModel, Employee } from '../../model/role';
 import { Client } from '../../model/class/Client';
@@ -13,7 +18,10 @@ import { Client } from '../../model/class/Client';
 export class ClientProjectComponent implements OnInit {
   projectForm: FormGroup = new FormGroup({
     clientProjectId: new FormControl(0),
-    projectName: new FormControl(''),
+    projectName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
     startDate: new FormControl(''),
     expectedEndDate: new FormControl(''),
     leadByEmpId: new FormControl(''),
