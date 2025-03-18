3388ba55e5985465b44f7ad743990e3dcb196e5e
gurparshadsharma <gurparshadsharma97@gmail.com>
2025-03-18 21:02:42 +0200

    changes

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  router = inject(Router);
+
+  onLogin() {
+    // after user click login button this method runs.
+    this.router.navigateByUrl('/client');
+  }
+}
